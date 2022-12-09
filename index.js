fef9c01101f62b0d1c459512ee41a2b9ca9daf4d
Elvis Njoroge Gitau <elvis.gitau@student.moringaschool.com>
2022-12-09 21:37:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  
+
+  for(let i = 0; i < array.length-1; i++){
+   
+    for(let j = i+1; j < array.length; j++){
+        
+      if(array[i]+array[j] === target){
+        return true
+      }
+    }
+  } 
+
+  return false;
 }
+
+
+  
+  
 
 /* 
   Write the Big O time complexity of your function here
+
+
+  n+n+1=(2n+1)=0(n)
 */
 
 /* 
   Add your pseudocode here
+  
+    for element i in array
+    for each other element j in the array
+      If their sum matches the target sum
+        return true
+  If none of the sums match the target sum
+    return false
 */
 
 /*
